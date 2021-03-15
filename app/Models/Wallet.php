9c934cf042e3b8b2f5c3123074fc88cb0141ae03
pankaj <pankaj@vm-jenkins-new.com>
2021-03-15 15:30:22 +0530

    minor fix in subcategory api

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -97,7 +97,7 @@ class Wallet extends Model
 
     public function customer()
     {
-        return $this->belongsTo('App\User', 'user_id');
+        return $this->belongsTo('App\Models\User', 'user_id');
     }
 
     public static function calculateEligibleCashback($price_total, $cashback_available){
