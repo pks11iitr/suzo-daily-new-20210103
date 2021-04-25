dd56b391a9033f8ecb608a5d080d23000c7790be
pankaj <pankaj@vm-jenkins-new.com>
2021-04-25 12:08:32 +0530

    log order cancel order

--- app/Events/LogOrder.php
+++ app/Events/LogOrder.php
@@ -2,6 +2,7 @@
 
 namespace App\Events;
 
+use App\Models\Order;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
@@ -13,15 +14,16 @@ use Illuminate\Queue\SerializesModels;
 class LogOrder
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
+     public $order;
 
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Order $order)
     {
-        //
+        $this->order=$order;
     }
 
     /**
